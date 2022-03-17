3433d960d840c64c99250e79bf1282038509d737
roytam1 <roytam@gmail.com>
2022-03-17 15:16:31 +0800

    import from UXP: Bug 1320408 - Part 19: Remove JSContext* parameter from ModuleObject::fixEnvironmentsAfterCompartmentMerge. (62eeeb76)

--- js/src/builtin/ModuleObject.cpp
+++ js/src/builtin/ModuleObject.cpp
@@ -768,7 +768,7 @@ AssertModuleScopesMatch(ModuleObject* module)
 }
 
 void
-ModuleObject::fixEnvironmentsAfterCompartmentMerge(JSContext* cx)
+ModuleObject::fixEnvironmentsAfterCompartmentMerge()
 {
     AssertModuleScopesMatch(this);
     initialEnvironment().fixEnclosingEnvironmentAfterCompartmentMerge(script()->global());

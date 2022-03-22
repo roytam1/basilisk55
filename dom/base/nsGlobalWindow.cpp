51d02c70aa9d95383a54702cc4f0979744cfe49a
roytam1 <roytam@gmail.com>
2022-03-22 14:17:36 +0800

    import from UXP: Bug 1341693 - Don't need to check GetDocShell() when creating CustomElementRegistry; (62678b52)

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -4079,8 +4079,9 @@ CustomElementRegistry*
 nsGlobalWindow::CustomElements()
 {
   MOZ_RELEASE_ASSERT(IsInnerWindow());
+
   if (!mCustomElements) {
-      mCustomElements = CustomElementRegistry::Create(AsInner());
+    mCustomElements = new CustomElementRegistry(AsInner());
   }
 
   return mCustomElements;

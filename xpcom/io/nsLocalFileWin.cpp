6291a065939b5a6b1ac3dd991211bc0e68ac76ee
roytam1 <roytam@gmail.com>
2022-04-03 15:51:44 +0800

    import from UXP: Revert "Treat *.jnlp as an executable class file, like *.jar" (512a3ba8)

--- xpcom/io/nsLocalFileWin.cpp
+++ xpcom/io/nsLocalFileWin.cpp
@@ -3036,7 +3036,6 @@ nsLocalFile::IsExecutable(bool* aResult)
       "ins",
       "isp",
       "jar",         // java application bundle
-      "jnlp",        // java web start
       "js",
       "jse",
       "lnk",

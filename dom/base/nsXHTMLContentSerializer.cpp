50809ff017ea156e1427a96d8fd8bb50623386cb
roytam1 <roytam@gmail.com>
2022-04-05 15:12:45 +0800

    import from UXP: Issue #1221: Pass the original element into nsXMLContentSerializer::CheckElementEnd so that we can properly determine whether it has children. (f34f8ca0)

--- dom/base/nsXHTMLContentSerializer.cpp
+++ dom/base/nsXHTMLContentSerializer.cpp
@@ -513,6 +513,7 @@ nsXHTMLContentSerializer::CheckElementStart(nsIContent * aContent,
 
 bool
 nsXHTMLContentSerializer::CheckElementEnd(mozilla::dom::Element* aElement,
+                                          mozilla::dom::Element* aOriginalElement,
                                           bool& aForceFormat,
                                           nsAString& aStr)
 {
@@ -531,7 +532,7 @@ nsXHTMLContentSerializer::CheckElementEnd(mozilla::dom::Element* aElement,
   }
 
   bool dummyFormat;
-  return nsXMLContentSerializer::CheckElementEnd(aElement, dummyFormat, aStr);
+  return nsXMLContentSerializer::CheckElementEnd(aElement, aOriginalElement, dummyFormat, aStr);
 }
 
 bool

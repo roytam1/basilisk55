315bc41d3ae51e7d7537a1e38632f995baf3345e
roytam1 <roytam@gmail.com>
2022-04-06 09:40:03 +0800

    import from UXP: Issue #1064 - Part 3: Fix notifyObservers() call. (24645d4b)

--- security/manager/pki/resources/content/exceptionDialog.js
+++ security/manager/pki/resources/content/exceptionDialog.js
@@ -246,7 +246,7 @@ function updateCertStatus() {
     document.getElementById("viewCertButton").disabled = false;
 
     // Notify observers about the availability of the certificate
-    Services.obs.notifyObservers(null, "cert-exception-ui-ready");
+    Services.obs.notifyObservers(null, "cert-exception-ui-ready", null);
   } else if (gChecking) {
     shortDesc = "addExceptionCheckingShort";
     longDesc  = "addExceptionCheckingLong2";

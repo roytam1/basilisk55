345c8d363676186bf81547b40b91120d34695299
roytam1 <roytam@gmail.com>
2022-04-06 11:15:28 +0800

    import from UXP: Issue #1118 - Part 7: Remove no-longer-used mWillReparent debug code. (10e59cb5)

--- dom/base/nsDocument.h
+++ dom/base/nsDocument.h
@@ -1681,11 +1681,6 @@ private:
   // Set to true when the document is possibly controlled by the ServiceWorker.
   // Used to prevent multiple requests to ServiceWorkerManager.
   bool mMaybeServiceWorkerControlled;
-
-#ifdef DEBUG
-public:
-  bool mWillReparent;
-#endif
 };
 
 class nsDocumentOnStack

a9a961b7003d9dba8e7a2f8cc6fd22c458d0e73a
roytam1 <roytam@gmail.com>
2022-04-06 14:56:26 +0800

    import from UXP: Bug 1301024 - Part 1: Set CreateElement/CreateElementNS is attribute. (054c852f)

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -5570,6 +5570,10 @@ nsDocument::CreateElement(const nsAString& aTagName,
   RefPtr<Element> elem = CreateElem(
     needsLowercase ? lcTagName : aTagName, nullptr, mDefaultElementType, is);
 
+  if (is) {
+    elem->SetAttr(kNameSpaceID_None, nsGkAtoms::is, *is, true);
+  }
+
   return elem.forget();
 }
 
@@ -5612,6 +5616,10 @@ nsDocument::CreateElementNS(const nsAString& aNamespaceURI,
                      NOT_FROM_PARSER, is);
   if (rv.Failed()) {
     return nullptr;
+  }
+
+  if (is) {
+    element->SetAttr(kNameSpaceID_None, nsGkAtoms::is, *is, true);
   }
 
   return element.forget();

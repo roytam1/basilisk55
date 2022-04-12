2e587ee18fde529ce395b9cbeb59e869006d355a
roytam1 <roytam@gmail.com>
2022-04-12 13:52:58 +0800

    import from UXP: Issue #1375 - Overload FromContent() to work with const (484b4810)

--- dom/base/nsIContent.h
+++ dom/base/nsIContent.h
@@ -1020,7 +1020,15 @@ inline nsIContent* nsINode::AsContent()
   {                                                                            \
     return aContent->_check ? static_cast<_class*>(aContent) : nullptr;        \
   }                                                                            \
+  static const _class* FromContent(const nsIContent* aContent)                 \
+  {                                                                            \
+    return aContent->_check ? static_cast<const _class*>(aContent) : nullptr;  \
+  }                                                                            \
   static _class* FromContentOrNull(nsIContent* aContent)                       \
+  {                                                                            \
+    return aContent ? FromContent(aContent) : nullptr;                         \
+  }                                                                            \
+  static const _class* FromContentOrNull(const nsIContent* aContent)           \
   {                                                                            \
     return aContent ? FromContent(aContent) : nullptr;                         \
   }

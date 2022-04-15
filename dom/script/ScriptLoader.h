b8935d5c9c793cdd220ca0e129b28ca3de7238c4
roytam1 <roytam@gmail.com>
2022-04-15 23:35:08 +0800

    import from UXP: Issue #618 - Split SRI verification out from OnStreamComplete. (6c7ac2b8)

--- dom/script/ScriptLoader.h
+++ dom/script/ScriptLoader.h
@@ -419,8 +419,6 @@ public:
                             mozilla::Vector<char16_t> &aString,
                             mozilla::dom::SRICheckDataVerifier* aSRIDataVerifier);
 
-  void HandleLoadError(ScriptLoadRequest *aRequest, nsresult aResult);
-
   /**
    * Processes any pending requests that are ready for processing.
    */
@@ -527,6 +525,8 @@ private:
   nsresult StartLoad(ScriptLoadRequest *aRequest, const nsAString &aType,
                      bool aScriptFromHead);
 
+  void HandleLoadError(ScriptLoadRequest *aRequest, nsresult aResult);
+
   /**
    * Process any pending requests asynchronously (i.e. off an event) if there
    * are any. Note that this is a no-op if there aren't any currently pending
@@ -560,6 +560,11 @@ private:
   {
     return mEnabled && !mBlockerCount;
   }
+
+  nsresult VerifySRI(ScriptLoadRequest *aRequest,
+                     nsIIncrementalStreamLoader* aLoader,
+                     nsresult aSRIStatus,
+                     SRICheckDataVerifier* aSRIDataVerifier) const;
 
   nsresult AttemptAsyncScriptCompile(ScriptLoadRequest* aRequest);
   nsresult ProcessRequest(ScriptLoadRequest* aRequest);

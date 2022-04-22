e80d4c7f97284e31bae53411cd34168126b6f975
roytam1 <roytam@gmail.com>
2022-04-22 22:47:03 +0800

    import from UXP: Issue #1515 - Add null check to nsCSSFrameConstructor::IsValidSibling (5fb8c296)

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -6483,6 +6483,11 @@ nsCSSFrameConstructor::IsValidSibling(nsIFrame*              aSibling,
                                       nsIContent*            aContent,
                                       StyleDisplay&          aDisplay)
 {
+  if (!aSibling || !aContent) {
+    // If either of these are null, no sane comparison can be made.
+    return false;
+  }
+  
   nsIFrame* parentFrame = aSibling->GetParent();
   nsIAtom* parentType = parentFrame->GetType();
 

f66b00b1b62631d3ef039c63c89b426b4c8e30d2
roytam1 <roytam@gmail.com>
2022-04-29 23:05:44 +0800

    import from UXP: [XPCOM] xpcom/threads misc cleanup (7de4e867)

--- xpcom/threads/nsProcessCommon.cpp
+++ xpcom/threads/nsProcessCommon.cpp
@@ -328,10 +328,13 @@ nsProcess::ProcessComplete()
   }
 
   const char* topic;
-  if (mExitValue < 0) {
-    topic = "process-failed";
-  } else {
-    topic = "process-finished";
+  { // Lock scope
+    MutexAutoLock lock(mLock);
+    if (mExitValue < 0) {
+      topic = "process-failed";
+    } else {
+      topic = "process-finished";
+    }
   }
 
   mPid = -1;
@@ -460,8 +463,11 @@ nsProcess::RunProcess(bool aBlocking, char** aMyArgv, nsIObserver* aObserver,
     }
   }
 
-  mExitValue = -1;
-  mPid = -1;
+  {
+    MutexAutoLock lock(mLock);
+    mExitValue = -1;
+    mPid = -1;
+  }
 
 #if defined(PROCESSMODEL_WINAPI)
   BOOL retVal;
@@ -553,6 +559,7 @@ nsProcess::RunProcess(bool aBlocking, char** aMyArgv, nsIObserver* aObserver,
   mBlocking = aBlocking;
   if (aBlocking) {
     Monitor(this);
+    MutexAutoLock lock(mLock);
     if (mExitValue < 0) {
       return NS_ERROR_FILE_EXECUTION_FAILED;
     }

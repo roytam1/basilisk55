2e3d4dfe12bf3c7ad0844c849be042553925bdb7
roytam1 <roytam@gmail.com>
2022-09-07 14:51:18 +0800

    import from UXP: Issue #1720 - Follow-up: Explicitly include X11UndefineNone.h in HTMLOptGroupElement.cpp to fix unified building on X11 (142f08ce)

--- dom/html/HTMLOptGroupElement.cpp
+++ dom/html/HTMLOptGroupElement.cpp
@@ -14,6 +14,10 @@
 #include "nsIFrame.h"
 #include "nsIFormControlFrame.h"
 
+#if defined(MOZ_X11)
+#include "X11UndefineNone.h"
+#endif
+
 NS_IMPL_NS_NEW_HTML_ELEMENT(OptGroup)
 
 namespace mozilla {

c47e9f0853364d0dae756284589ed12e2aff2bdc
roytam1 <roytam@gmail.com>
2022-12-03 00:46:56 +0800

    ported from UXP: Issue #2030 - Dispatch click on common interactive ancestor if mousdown/up are not on the same element (0494d561c)

--- dom/events/EventStateManager.h
+++ dom/events/EventStateManager.h
@@ -1015,11 +1015,8 @@ private:
   uint16_t mGestureDownButtons;
 
   nsCOMPtr<nsIContent> mLastLeftMouseDownContent;
-  nsCOMPtr<nsIContent> mLastLeftMouseDownContentParent;
   nsCOMPtr<nsIContent> mLastMiddleMouseDownContent;
-  nsCOMPtr<nsIContent> mLastMiddleMouseDownContentParent;
   nsCOMPtr<nsIContent> mLastRightMouseDownContent;
-  nsCOMPtr<nsIContent> mLastRightMouseDownContentParent;
 
   nsCOMPtr<nsIContent> mActiveContent;
   nsCOMPtr<nsIContent> mHoverContent;

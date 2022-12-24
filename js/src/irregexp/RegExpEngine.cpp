a56660b7281c71e1a5ffd8e832e92aa337fe9819
roytam1 <roytam@gmail.com>
2022-12-24 08:35:09 +0800

    import from UXP: Issue #1279 - Implement regular expressions lookbehind (v2) (2f0896ab)

--- js/src/irregexp/RegExpEngine.cpp
+++ js/src/irregexp/RegExpEngine.cpp
@@ -4860,6 +4860,9 @@ TextNode::GetQuickCheckDetails(QuickCheckDetails* details,
                                int characters_filled_in,
                                bool not_at_start)
 {
+    // Do not collect any quick check details if the text node reads backward,
+    // since it reads in the opposite direction than we use for quick checks.
+    if (read_backward()) return;
     MOZ_ASSERT(characters_filled_in < details->characters());
     int characters = details->characters();
     int char_mask = MaximumCharacter(compiler->ascii());

59297b6be697aab9bde29ba993f80586af8e6c80
roytam1 <roytam@gmail.com>
2023-01-19 09:51:13 +0800

    import from UXP: [DOM] Make nsHTMLContentSerializer handle noscript depending on the scripting enabled state of the document. (f080f2e9)

--- dom/base/nsHTMLContentSerializer.cpp
+++ dom/base/nsHTMLContentSerializer.cpp
@@ -285,7 +285,7 @@ nsHTMLContentSerializer::AppendElementStart(Element* aElement,
   if (ns == kNameSpaceID_XHTML &&
       (name == nsGkAtoms::script ||
        name == nsGkAtoms::style ||
-       name == nsGkAtoms::noscript ||
+       (name == nsGkAtoms::noscript && aElement->OwnerDoc()->IsScriptEnabled()) ||
        name == nsGkAtoms::noframes)) {
     ++mDisableEntityEncoding;
   }
@@ -315,7 +315,7 @@ nsHTMLContentSerializer::AppendElementEnd(Element* aElement,
   if (ns == kNameSpaceID_XHTML &&
       (name == nsGkAtoms::script ||
        name == nsGkAtoms::style ||
-       name == nsGkAtoms::noscript ||
+       (name == nsGkAtoms::noscript && aElement->OwnerDoc()->IsScriptEnabled()) ||
        name == nsGkAtoms::noframes)) {
     --mDisableEntityEncoding;
   }

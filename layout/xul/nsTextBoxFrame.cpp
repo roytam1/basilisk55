729323e8d97b52a527cd8d7ea65454a17820a26a
roytam1 <roytam@gmail.com>
2023-02-08 17:36:20 +0800

    import from UXP: Issue #2104 - Part 1: Implement "clip" attribute value for XUL string cropping (60c344a5)

--- layout/xul/nsTextBoxFrame.cpp
+++ layout/xul/nsTextBoxFrame.cpp
@@ -233,7 +233,8 @@ nsTextBoxFrame::UpdateAttributes(nsIAtom*         aAttribute,
     if (aAttribute == nullptr || aAttribute == nsGkAtoms::crop) {
         static nsIContent::AttrValuesArray strings[] =
           {&nsGkAtoms::left, &nsGkAtoms::start, &nsGkAtoms::center,
-           &nsGkAtoms::right, &nsGkAtoms::end, &nsGkAtoms::none, nullptr};
+           &nsGkAtoms::right, &nsGkAtoms::end, &nsGkAtoms::none,
+           &nsGkAtoms::clip, nullptr};
         CroppingStyle cropType;
         switch (mContent->FindAttrValueIn(kNameSpaceID_None, nsGkAtoms::crop,
                                           strings, eCaseMatters)) {
@@ -250,6 +251,9 @@ nsTextBoxFrame::UpdateAttributes(nsIAtom*         aAttribute,
             break;
           case 5:
             cropType = CropNone;
+            break;
+          case 6:
+            cropType = CropClip;
             break;
           default:
             cropType = CropAuto;
@@ -650,7 +654,9 @@ nsTextBoxFrame::CalculateTitleForWidth(nsRenderingContext& aRenderingContext,
     }
 
     const nsDependentString& kEllipsis = nsContentUtils::GetLocalizedEllipsis();
-    if (mCropType != CropNone) {
+    if (mCropType == CropClip) {
+      mCroppedTitle.Truncate();
+    } else if (mCropType != CropNone) {
       // start with an ellipsis
       mCroppedTitle.Assign(kEllipsis);
 
@@ -684,6 +690,7 @@ nsTextBoxFrame::CalculateTitleForWidth(nsRenderingContext& aRenderingContext,
         case CropAuto:
         case CropNone:
         case CropRight:
+        case CropClip:
         {
             ClusterIterator iter(mTitle.Data(), mTitle.Length());
             const char16_t* dataBegin = iter;

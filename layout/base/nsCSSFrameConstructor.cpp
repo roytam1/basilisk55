a3c668386e75ad7a92efaf5fe1892d8895e03997
roytam1 <roytam@gmail.com>
2023-02-28 10:58:53 +0800

    ported from UXP: Issue #1375 - Follow-up: Get the insertion point right when reconstructing direct children of a shadow root (b40c87a5)

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -7431,7 +7431,9 @@ nsCSSFrameConstructor::ContentAppended(nsIContent*     aContainer,
                                 GetAbsoluteContainingBlock(parentFrame, FIXED_POS),
                                 GetAbsoluteContainingBlock(parentFrame, ABS_POS),
                                 containingBlock);
-  state.mTreeMatchContext.InitAncestors(aContainer->AsElement());
+  // We use GetParentElementCrossingShadowRoot to handle the case where
+  // aContainer is a ShadowRoot.
+  state.mTreeMatchContext.InitAncestors(aFirstNewContent->GetParentElementCrossingShadowRoot());
 
   nsIAtom* frameType = parentFrame->GetType();
 
@@ -7772,7 +7774,8 @@ nsCSSFrameConstructor::ContentRangeInserted(nsIContent*            aContainer,
     // The xbl:children element won't have a frame, but default content can have the children as
     // a parent. While its uncommon to change the structure of the default content itself, a label,
     // for example, can be reframed by having its value attribute set or removed.
-    if (!parentFrame && !aContainer->IsActiveChildrenElement()) {
+    if (!parentFrame &&
+        !(aContainer->IsActiveChildrenElement() || ShadowRoot::FromNode(aContainer))) {
       // We're punting on frame construction because there's no container frame.
       // The Servo-backed style system handles this case like the lazy frame
       // construction case.
@@ -7781,6 +7784,8 @@ nsCSSFrameConstructor::ContentRangeInserted(nsIContent*            aContainer,
       }
       return;
     }
+
+    MOZ_ASSERT_IF(ShadowRoot::FromNode(aContainer), !parentFrame);
 
     // Otherwise, we've got parent content. Find its frame.
     NS_ASSERTION(!parentFrame || parentFrame->GetContent() == aContainer ||
@@ -7898,9 +7903,9 @@ nsCSSFrameConstructor::ContentRangeInserted(nsIContent*            aContainer,
                                 GetAbsoluteContainingBlock(insertion.mParentFrame, ABS_POS),
                                 GetFloatContainingBlock(insertion.mParentFrame),
                                 do_AddRef(aFrameState));
-  state.mTreeMatchContext.InitAncestors(aContainer ?
-                                          aContainer->AsElement() :
-                                          nullptr);
+  // We use GetParentElementCrossingShadowRoot to handle the case where
+  // aContainer is a ShadowRoot.
+  state.mTreeMatchContext.InitAncestors(aStartChild->GetParentElementCrossingShadowRoot());
 
   // Recover state for the containing block - we need to know if
   // it has :first-letter or :first-line style applied to it. The
@@ -9082,8 +9087,11 @@ nsCSSFrameConstructor::GetInsertionPoint(nsIContent* aContainer,
       return InsertionPoint(GetContentInsertionFrameFor(aContainer), aContainer);
     }
 
-    if (nsContentUtils::HasDistributedChildren(aContainer)) {
-      // The container distributes nodes, use the frame of the flattened tree parent.
+    if (nsContentUtils::HasDistributedChildren(aContainer) ||
+        ShadowRoot::FromNode(aContainer)) {
+      // The container distributes nodes or is a shadow root, use the frame of
+      // the flattened tree parent.
+      //
       // It may be the case that the node is distributed but not matched to any
       // insertion points, so there is no flattened parent.
       nsIContent* flattenedParent = aChild->GetFlattenedTreeParent();

d1e5efcf8523ef36f9fc214086e6a9288c0b8c1d
roytam1 <roytam@gmail.com>
2023-03-07 11:01:12 +0800

    ported from UXP: Issue #2135 - Bug 1433669/Part 2: Flush the document instead of the shell in ContentEventHandler (4d778226)

--- dom/events/ContentEventHandler.cpp
+++ dom/events/ContentEventHandler.cpp
@@ -115,12 +115,9 @@ ContentEventHandler::InitBasic()
 {
   NS_ENSURE_TRUE(mPresShell, NS_ERROR_NOT_AVAILABLE);
 
-  // If text frame which has overflowing selection underline is dirty,
-  // we need to flush the pending reflow here.
-  mPresShell->FlushPendingNotifications(FlushType::Layout);
-
-  // Flushing notifications can cause mPresShell to be destroyed (bug 577963).
-  NS_ENSURE_TRUE(!mPresShell->IsDestroying(), NS_ERROR_FAILURE);
+  if (nsIDocument* doc = mPresShell->GetDocument()) {
+    doc->FlushPendingNotifications(FlushType::Layout);
+  }
 
   return NS_OK;
 }

fa81bc7c93a21505bd549652e407e2fc8838db61
roytam1 <roytam@gmail.com>
2023-03-07 10:21:22 +0800

    ported from UXP: Issue #2135 - Implement sequential focus navigation for shadow DOM (56193120)

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -6480,9 +6480,7 @@ nsContentUtils::IsFocusedContent(const nsIContent* aContent)
 bool
 nsContentUtils::IsSubDocumentTabbable(nsIContent* aContent)
 {
-  //XXXsmaug Shadow DOM spec issue!
-  //         We may need to change this to GetComposedDoc().
-  nsIDocument* doc = aContent->GetUncomposedDoc();
+  nsIDocument* doc = aContent->GetComposedDoc();
   if (!doc) {
     return false;
   }

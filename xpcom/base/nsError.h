a0721af93b93d5a7f858756a2ee442960ecdbf32
roytam1 <roytam@gmail.com>
2023-04-12 11:19:29 +0800

    ported from UXP: Issue #2053 - Part 5: Throw a DOMException instead of a JS exception for some errors (ef8e3b54)

--- xpcom/base/nsError.h
+++ xpcom/base/nsError.h
@@ -81,6 +81,7 @@
 #define NS_ERROR_MODULE_DOM_PUSH 40
 #define NS_ERROR_MODULE_DOM_MEDIA 41
 #define NS_ERROR_MODULE_URL_CLASSIFIER 42
+#define NS_ERROR_MODULE_DOM_USER_TIMING 43
 
 /* NS_ERROR_MODULE_GENERAL should be used by modules that do not
  * care if return code values overlap. Callers of methods that

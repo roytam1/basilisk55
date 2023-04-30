b83cfd9ba018fb9c0c78cf3a8470837b1fc04714
roytam1 <roytam@gmail.com>
2023-05-01 00:20:28 +0800

    import from UXP: Issue #1691 - Part 10: Add and use method to annotate CC crashes with a class name. M1277260 Make PtrInfo into a class and mark it final. Also fix an erroneous debug assert because mBaseURL not set in one code path. (7e056a6f)

--- dom/script/ScriptLoader.cpp
+++ dom/script/ScriptLoader.cpp
@@ -1767,10 +1767,10 @@ ScriptLoader::ProcessScriptElement(nsIScriptElement *aElement)
   MOZ_ASSERT_IF(!request->IsModuleRequest(), !aElement->GetScriptAsync());
   request->SetScriptMode(false, aElement->GetScriptAsync());
 
+  request->mBaseURL = mDocument->GetDocBaseURI();
+
   if (request->IsModuleRequest()) {
     ModuleLoadRequest* modReq = request->AsModuleRequest();
-
-    request->mBaseURL = mDocument->GetDocBaseURI();
 
     if (aElement->GetScriptAsync()) {
       AddAsyncRequest(modReq);

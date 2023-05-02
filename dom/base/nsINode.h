8921a9b24bd14ddb8356452e3d3e05d0ebf74409
roytam1 <roytam@gmail.com>
2023-05-03 00:17:15 +0800

    import from UXP: Issue #2225 - Implement Element.replaceChildren (ae50e9e0)

--- dom/base/nsINode.h
+++ dom/base/nsINode.h
@@ -1924,6 +1924,7 @@ public:
 
   void Prepend(const Sequence<OwningNodeOrString>& aNodes, ErrorResult& aRv);
   void Append(const Sequence<OwningNodeOrString>& aNodes, ErrorResult& aRv);
+  void ReplaceChildren(const Sequence<OwningNodeOrString>& aNodes, ErrorResult& aRv);
 
   void GetBoxQuads(const BoxQuadOptions& aOptions,
                    nsTArray<RefPtr<DOMQuad> >& aResult,

c03ea8fbf07d504ecea1eca3c1c80b5d61e04a42
roytam1 <roytam@gmail.com>
2023-06-26 11:51:30 +0800

    ported from UXP: - Issue #1769 - Part 2: Implement JPEG-XL decoder and about:config and MIME plumbing. (51ea0e4f) - Issue #1769 - Part 3: Cleanup nsJXLDecoder. (2df55850) - Issue #1769 - Part 2 Follow-up: Do not use namespace parent::child {} for defining nested namespaces. (134c5e94) - Issue #1769: Ensure MIME type is known in URIloader for jxl (f860413e)

--- gfx/thebes/gfxPrefs.h
+++ gfx/thebes/gfxPrefs.h
@@ -450,6 +450,9 @@ private:
   DECL_GFX_PREF(Once, "image.mem.surfacecache.size_factor",    ImageMemSurfaceCacheSizeFactor, uint32_t, 64);
   DECL_GFX_PREF(Once, "image.multithreaded_decoding.limit",    ImageMTDecodingLimit, int32_t, -1);
   DECL_GFX_PREF(Live, "image.webp.enabled",                    ImageWebPEnabled, bool, true);
+#ifdef MOZ_JXL
+  DECL_GFX_PREF(Live, "image.jxl.enabled",                     ImageJXLEnabled, bool, true);
+#endif
 
   DECL_GFX_PREF(Once, "layers.acceleration.enabled",           LayersAccelerationEnabledDoNotUseDirectly, bool, true);
   DECL_GFX_PREF(Live, "layers.acceleration.draw-fps",          LayersDrawFPS, bool, false);
---
+++ image/decoders/nsJXLDecoder.h
@@ -0,0 +1,52 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_image_decoders_nsJXLDecoder_h
+#define mozilla_image_decoders_nsJXLDecoder_h
+
+#include "Decoder.h"
+#include "SurfacePipe.h"
+
+#include "jxl/decode_cxx.h"
+#include "jxl/thread_parallel_runner_cxx.h"
+
+namespace mozilla {
+namespace image {
+class RasterImage;
+
+class nsJXLDecoder final : public Decoder {
+ public:
+  virtual ~nsJXLDecoder();
+
+ protected:
+  LexerResult DoDecode(SourceBufferIterator& aIterator,
+                       IResumable* aOnResume) override;
+
+ private:
+  friend class DecoderFactory;
+
+  // Decoders should only be instantiated via DecoderFactory.
+  explicit nsJXLDecoder(RasterImage* aImage);
+
+  size_t PreferredThreadCount();
+
+  enum class State { JXL_DATA, FINISHED_JXL_DATA };
+
+  LexerTransition<State> ReadJXLData(const char* aData, size_t aLength);
+  LexerTransition<State> FinishedJXLData();
+
+  StreamingLexer<State> mLexer;
+  JxlDecoderPtr mDecoder;
+  JxlThreadParallelRunnerPtr mParallelRunner;
+  Vector<uint8_t> mBuffer;
+  Vector<uint8_t> mOutBuffer;
+  JxlBasicInfo mInfo{};
+};
+
+}  // namespace image
+}  // namespace mozilla
+
+#endif  // mozilla_image_decoders_nsJXLDecoder_h

8612d9045b7d30d7340ffee64d927bcf5d9f53da
roytam1 <roytam@gmail.com>
2023-08-17 09:50:09 +0800

    import from UXP: No Issue - Fix building WebRTC/Basilisk with --enable-debug A bogus MOZ_ASSERT was added at some point, this should make it work. (013f130e)

--- media/webrtc/signaling/src/media-conduit/VideoConduit.cpp
+++ media/webrtc/signaling/src/media-conduit/VideoConduit.cpp
@@ -239,7 +239,7 @@ WebrtcVideoConduit::~WebrtcVideoConduit()
 
   // Release AudioConduit first by dropping reference on MainThread, where it expects to be
   SyncTo(nullptr);
-  MOZ_ASSERT(!mSendStream && !mRecvStream, "Call DeleteStreams prior to ~WebrtcVideoConduit.");
+  MOZ_ASSERT(!mPtrViEBase, "Call DeleteStreams prior to ~WebrtcVideoConduit.");
 }
 
 void

c97c310662827256ff0c524b28d852f8ac6aa403
roytam1 <roytam@gmail.com>
2024-01-05 10:24:48 +0800

    import from UXP: Debug: Remove assertion for nsDOMNavigationTiming. (644b8680)

--- dom/base/nsDOMNavigationTiming.h
+++ dom/base/nsDOMNavigationTiming.h
@@ -163,7 +163,6 @@ public:
 
   inline DOMHighResTimeStamp TimeStampToDOMHighRes(mozilla::TimeStamp aStamp) const
   {
-    MOZ_ASSERT(!aStamp.IsNull(), "The timestamp should not be null");
     if (aStamp.IsNull()) {
       return 0;
     }

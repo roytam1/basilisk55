1c659ecbe3a99fe369340524971327f1c85120b3
roytam1 <roytam@gmail.com>
2024-04-16 15:21:00 +0800

    import from UXP: [DOM] Add nullcheck on mDoc in GetEventTargetParent (2811d300)

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -3528,6 +3528,8 @@ nsGlobalWindow::GetEventTargetParent(EventChainPreVisitor& aVisitor)
 
   aVisitor.mCanHandle = true;
   // Middle/right click shouldn't dispatch click event, use auxclick to instead.
+  // Note: mDoc should always exist here, but check just in case someone yanked
+  // it out from under us.
   if (mDoc && mDoc->IsXULDocument()) {
     aVisitor.mForceContentDispatch = true; //FIXME! Bug 329119
   }

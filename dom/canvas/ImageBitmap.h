d59239f54a4a36211accbd65fb190e6d84dee4ab
roytam1 <roytam@gmail.com>
2024-07-10 17:33:47 +0800

    import from UXP: Issue #2546 - Part 6: Remove canvas.imagebitmap_extensions.enabled preference. (7cc4dcb6)

--- dom/canvas/ImageBitmap.h
+++ dom/canvas/ImageBitmap.h
@@ -140,9 +140,6 @@ public:
                        nsTArray<RefPtr<gfx::DataSourceSurface>>& aClonedSurfaces,
                        ImageBitmap* aImageBitmap);
 
-  // Mozilla Extensions
-  static bool ExtensionsEnabled(JSContext* aCx, JSObject* aObj);
-
   friend CreateImageBitmapFromBlob;
   friend CreateImageBitmapFromBlobTask;
   friend CreateImageBitmapFromBlobWorkerTask;

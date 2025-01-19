fa638329cde817e9234a62974be23cc7b9d4227b
roytam1 <roytam@gmail.com>
2025-01-20 07:32:12 +0800

    import from UXP: [network] Don't try to use non-existing connections. (624ff30b)

--- netwerk/protocol/http/nsHttpTransaction.cpp
+++ netwerk/protocol/http/nsHttpTransaction.cpp
@@ -761,7 +761,7 @@ nsHttpTransaction::ReadSegments(nsAHttpSegmentReader *reader,
         return mStatus;
     }
 
-    if (!mConnected && !m0RTTInProgress) {
+    if (mConnection && !mConnected && !m0RTTInProgress) {
         mConnected = true;
         nsCOMPtr<nsISupports> info;
         mConnection->GetSecurityInfo(getter_AddRefs(info));

0226a7f4e982aa83aab5d72dee3e433f8881704d
roytam1 <roytam@gmail.com>
2025-02-06 10:45:20 +0800

    ported from UXP: Issue #2690 - Allow 0-length keys/empty passwords for PBKDF2. (e1e7a0c5)

--- dom/crypto/WebCryptoTask.cpp
+++ dom/crypto/WebCryptoTask.cpp
@@ -1630,7 +1630,8 @@ public:
     }
 
     // Check that we have valid key data.
-    if (mKeyData.Length() == 0) {
+    // Zero-length key is allowed for PBKDF2 since it will be padded.
+    if (mKeyData.Length() == 0 && !mAlgName.EqualsLiteral(WEBCRYPTO_ALG_PBKDF2)) {
       return NS_ERROR_DOM_DATA_ERR;
     }
 
@@ -2844,12 +2845,6 @@ public:
     Telemetry::Accumulate(Telemetry::WEBCRYPTO_ALG, TA_PBKDF2);
     CHECK_KEY_ALGORITHM(aKey.Algorithm(), WEBCRYPTO_ALG_PBKDF2);
 
-    // Check that we got a symmetric key
-    if (mSymKey.Length() == 0) {
-      mEarlyRv = NS_ERROR_DOM_INVALID_ACCESS_ERR;
-      return;
-    }
-
     RootedDictionary<Pbkdf2Params> params(aCx);
     mEarlyRv = Coerce(aCx, params, aAlgorithm);
     if (NS_FAILED(mEarlyRv)) {
@@ -2857,9 +2852,9 @@ public:
       return;
     }
 
-    // length must be a multiple of 8 bigger than zero.
+    // Length must be a multiple of 8 bigger than zero.
     if (aLength == 0 || aLength % 8) {
-      mEarlyRv = NS_ERROR_DOM_DATA_ERR;
+      mEarlyRv = NS_ERROR_DOM_OPERATION_ERR;
       return;
     }
 

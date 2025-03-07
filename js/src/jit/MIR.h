b8ac890575f312d521a489ba93de43e43d68fb15
roytam1 <roytam@gmail.com>
2025-03-07 22:07:07 +0800

    import from UXP: [js] Disable potentially unsafe attempts at recovering JIT operations. (43a2299)

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -7048,10 +7048,8 @@ class MRegExpMatcher
     TRIVIAL_NEW_WRAPPERS
     NAMED_OPERANDS((0, regexp), (1, string), (2, lastIndex))
 
-    MOZ_MUST_USE bool writeRecoverData(CompactBufferWriter& writer) const override;
-
     bool canRecoverOnBailout() const override {
-        return true;
+        return false;
     }
 
     bool possiblyCalls() const override {
@@ -7086,7 +7084,7 @@ class MRegExpSearcher
     MOZ_MUST_USE bool writeRecoverData(CompactBufferWriter& writer) const override;
 
     bool canRecoverOnBailout() const override {
-        return true;
+        return false;
     }
 
     bool possiblyCalls() const override {

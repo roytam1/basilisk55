a480b4db2386f48bec466a37b6628b0d7bbe86a2
roytam1 <roytam@gmail.com>
2026-01-15 23:00:06 +0800

    import from UXP: [DOM] Check worker script size (d8f72029)

--- dom/workers/Workers.h
+++ dom/workers/Workers.h
@@ -366,6 +366,11 @@ IsDebuggerGlobal(JSObject* global);
 bool
 IsDebuggerSandbox(JSObject* object);
 
+inline size_t GetWorkerScriptMaxSizeInBytes() {
+  // This is the max size that any of our worker scripts can be.
+  return (INT32_MAX - 1) / 4;
+}
+
 END_WORKERS_NAMESPACE
 
 #endif // mozilla_dom_workers_workers_h__

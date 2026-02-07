742be73a05607f82a835021d2da0462c9b6a8d7b
roytam1 <roytam@gmail.com>
2026-02-07 22:19:22 +0800

    import from UXP: Issue #2932 - Ensure that imgRequestProxy::CancelAndForgetObserver removes itself from the cache validator (1078e45d)

--- image/imgLoader.cpp
+++ image/imgLoader.cpp
@@ -2798,6 +2798,12 @@ imgCacheValidator::AddProxy(imgRequestProxy* aProxy)
   mProxies.AppendObject(aProxy);
 }
 
+void
+imgCacheValidator::RemoveProxy(imgRequestProxy* aProxy)
+{
+  mProxies.RemoveObject(aProxy);
+}
+
 /** nsIRequestObserver methods **/
 
 NS_IMETHODIMP
--- image/imgLoader.h
+++ image/imgLoader.h
@@ -525,6 +525,7 @@ public:
                     bool forcePrincipalCheckForCacheEntry);
 
   void AddProxy(imgRequestProxy* aProxy);
+  void RemoveProxy(imgRequestProxy* aProxy);
 
   NS_DECL_ISUPPORTS
   NS_DECL_NSITHREADRETARGETABLESTREAMLISTENER

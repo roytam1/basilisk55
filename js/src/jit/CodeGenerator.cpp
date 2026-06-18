456bea8af995c6ccfed66cfdcba8dd75d5dd05fa
roytam1 <roytam@gmail.com>
2026-06-18 17:10:35 +0800

    import from UXP: No issue - Use ImmGCPtr for module meta data visits. (0bdcc65c)

--- js/src/jit/CodeGenerator.cpp
+++ js/src/jit/CodeGenerator.cpp
@@ -2566,7 +2566,7 @@ static const VMFunction GetOrCreateModuleMetaObjectInfo =
 void
 CodeGenerator::visitModuleMetadata(LModuleMetadata* lir)
 {
-    pushArg(ImmPtr(lir->mir()->module()));
+    pushArg(ImmGCPtr(lir->mir()->module()));
     callVM(GetOrCreateModuleMetaObjectInfo, lir);
 }
 

2483193abce09677e4f1f4ce317e4de16bc22c86
roytam1 <roytam@gmail.com>
2026-07-31 17:13:44 +0800

    ported from UXP: Issue #2404 - Split aspect ratio enum types into a separate header to fix compilation issuesv (d6022d38)

--- layout/style/nsStyleCoord.h
+++ layout/style/nsStyleCoord.h
@@ -11,6 +11,7 @@
 #include <type_traits>
 
 #include "mozilla/EnumTypeTraits.h"
+#include "mozilla/LogicalTypes.h"
 #include "nsCoord.h"
 #include "nsStyleConsts.h"
 #include "nsTArray.h"
@@ -19,30 +20,6 @@
 namespace mozilla {
 
 class WritingMode;
-
-// Logical axis, edge, side and corner constants for use in various places.
-enum LogicalAxis {
-  eLogicalAxisBlock  = 0x0,
-  eLogicalAxisInline = 0x1
-};
-enum LogicalEdge {
-  eLogicalEdgeStart  = 0x0,
-  eLogicalEdgeEnd    = 0x1
-};
-enum LogicalSide {
-  eLogicalSideBStart = (eLogicalAxisBlock  << 1) | eLogicalEdgeStart,  // 0x0
-  eLogicalSideBEnd   = (eLogicalAxisBlock  << 1) | eLogicalEdgeEnd,    // 0x1
-  eLogicalSideIStart = (eLogicalAxisInline << 1) | eLogicalEdgeStart,  // 0x2
-  eLogicalSideIEnd   = (eLogicalAxisInline << 1) | eLogicalEdgeEnd     // 0x3
-};
-
-enum LogicalCorner
-{
-  eLogicalCornerBStartIStart = 0,
-  eLogicalCornerBStartIEnd   = 1,
-  eLogicalCornerBEndIEnd     = 2,
-  eLogicalCornerBEndIStart   = 3
-};
 
 } // namespace mozilla
 
